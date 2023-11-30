483f33cfba777313955406c6fcbe91e2d297f79d
rew2155 <135544795+rew2155@users.noreply.github.com>
2023-11-29 22:36:05 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-
 """
 Columbia's COMS W4111.001 Introduction to Databases
 Example Webserver
@@ -12,11 +11,13 @@ import os
   # accessible as a variable in index.html:
 from sqlalchemy import create_engine, text 
 from sqlalchemy.pool import NullPool
-from flask import Flask, jsonify, request, render_template, g, redirect, Response, abort
+from flask import Flask, jsonify, request, render_template, g, redirect, Response, flash, session, url_for
+import psycopg2, psycopg2.extras
+from urllib.parse import urlparse
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
-
+app.secret_key = 'bankprogram-milly'
 #
 # The following is a dummy URI that does not connect to a valid database. You will need to modify it to connect to your Part 2 database in order to use the data.
 #
@@ -31,17 +32,16 @@ app = Flask(__name__, template_folder=tmpl_dir)
 DATABASEURI = "postgresql://mki2104:908176@34.74.171.121/proj1part2"
 
 
-#
-# This line creates a database engine that knows how to connect to the URI above.
-#
+result = urlparse(DATABASEURI)
+username = result.username
+password = result.password
+database = result.path[1:]
+hostname = result.hostname
+port = result.port
+conn = psycopg2.connect( database = database, user = username, password = password, host = hostname, port = port )
+
 engine = create_engine(DATABASEURI, pool_pre_ping=True)
 
-#
-# Example of running queries in your database
-# Note that this will probably not work if you already have a table named 'test' in your database, containing meaningful data. This is only an example showing you how to run queries in your database using SQLAlchemy.
-#
-conn = engine.connect()
-conn.commit()
 @app.before_request
 def before_request():
   """
@@ -75,72 +75,163 @@ def teardown_request(exception):
 def index():
   return render_template("index.html")
 
-def isValidUni(uni):
-  with engine.connect() as conn:
-    uni_param = request.args.get('uni')
-    query = text("SELECT * FROM user_attends WHERE uni = :uni")
-    login_data = conn.execute(query, {"uni": uni_param}).fetchall()
-  return login_data > 0  # Valid if at least one record exists for the given uni
 
+# Endpoint for Login 
 @app.route('/login.html', methods=['POST', 'GET'])
 def login():
-  with engine.connect() as conn:
-    if request.method == 'POST':
-      uni = request.json.get('uni')
-      name = request.json.get('name')
-      if not isValidUni(uni):
-        return jsonify(valid=False)
-      else:
-          return redirect("/user_profile.html")
-    else:
-      return render_template("login.html")
+    cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    # Check if "username" and "password" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'uni' in request.form:
+        uni = request.form['uni']
+        username = request.form['username']
+        name = request.form.get("name", "")  # Use get() to handle the case where 'name' is not present
+        password = request.form['password']
+        
+        cursor.execute('SELECT username, password, uni, name FROM user_attends WHERE username = %s', (username,))
+        profile = cursor.fetchone()
+
+        if profile:
+            uni_rs = profile['uni']
+            # If account exists in users table in our database
+            if uni_rs == uni:
+                # Create session data, we can access this data in other routes
+                session['uni'] = profile['uni']
+                session['username'] = profile['username']
+                
+                # Check if 'name' key exists in the profile dictionary
+                if 'name' in profile:
+                    session['name'] = profile['name']
+                else:
+                    session['name'] = ""  # Provide a default value or handle it based on your application logic
+                
+                session['password'] = profile['password']
+
+                # Flash the 'info' dictionary and redirect
+                #flash({'uni': profile['uni'], 'name': session['name'], 'username': profile['username'], 'password': profile['password']})
+                
+                # Redirect to home page
+                return redirect(url_for('user_profile', name=session['name']))
+
+            else:
+                # Account doesn't exist or uni/password incorrect
+                flash('Incorrect uni or unknown account')
+        else:
+            # Account doesn't exist or uni/password incorrect
+            flash('Incorrect uni or unknown account')
+
+    # Add this return statement to handle the case where the conditions are not met
+    return render_template('login.html')
+
 
 # Endpoint for User Profile
-@app.route('/user_profile.html', methods=['POST', 'GET'])
-def user_profile():
-    with engine.connect() as conn:
-        if request.method == 'POST':
-            query = text("SELECT u.* FROM user_attends u JOIN institutions i ON u.uni = i.uni WHERE u.uni = :uni AND u.name = :name")
-            user_profile_data = conn.execute(query, {"name": name}).fetchall()
-            return render_template("user_profile.html", data=user_profile_data)  
-    return render_template("user_profile.html")
+@app.route('/user_profile.html/<name>', methods=['POST', 'GET'])
+def user_profile(name):
+    # Retrieve the data from the session
+    uni = session.get('uni', "")
+    #name = session.get('name', "")
+    username = session.get('username', "")
+    password = session.get('password', "")
+
+    #cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    query = text('SELECT u.schoolcode, i.school FROM user_attends u JOIN institutions i ON u.schoolcode = i.schoolcode WHERE u.uni = :uni ')
+    params = {'uni': uni}
+    result = g.conn.execute(query, params)
+    g.conn.commit()
+    #print(result)
+    for i in result:
+       schoolcode = i[0]
+       schoolname = i[1]
+       
+    #query = text('SELECT a.balance FROM')
+    #params = {'uni': uni}
+    #result = g.conn.execute(query, params)
+    #g.conn.commit()
+    #for j in result:
+    #   schoolcode = j[0]
+    #   schoolname = j[1]
+   
+    # Render the template with the retrieved data
+    return render_template("user_profile.html", info={'uni': uni, 'name': name, 'username': username, 'password': password, 
+            'schoolcode': schoolcode, 'schoolname': schoolname })
+    #'balance': balance, 'swipes': swipes,'dining_dollars': dining_dollars,'points': points, 'flex': flex})
+
+
+
 
 # Endpoint for Savings
-@app.route('/savings', methods=['POST'])
+@app.route('/savings', methods=['GET'])
 def savings():
-  with engine.connect() as conn:
-    savings_param =request.args.get('balance')
-    query = text("SELECT s.* FROM savings_account s JOIN account_belongsto ab ON s.accountid = ab.accountid WHERE ab.accountid = :accountid")
-    savingsAccount_data = conn.execute(query, {"balance": savings_param}).fetchall()
-  return savingsAccount_data
-      
-#@app.route('/checkings', methods=[ 'GET','POST'])
+    uni = session.get('uni', "")
+    #cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    query = text('SELECT ab.accountid, s.balance FROM savings_account s JOIN account_belongsto ab ON s.accountid = ab.accountid WHERE ab.uni = :uni')
+    params = {'uni': uni}
+    result = g.conn.execute(query, params)
+    g.conn.commit()
+
+    for i in result:
+       accountid = i[0]
+       balance = i[1]
+       
+    return render_template("user_profile.html", info = {'accountid': accountid, 'balance': balance})
+
+#Endpoint for Checkings      
+@app.route('/checkings', methods=['GET', 'POST'])
 def checkings():
-  with engine.connect() as conn:
-    checkings_param = request.args.get('balance')
-    query = text("SELECT c.* FROM checkings_account c JOIN account_belongsto ab ON c.accountid = ab.accountid WHERE ab.accountid = :accountid")
-    checkingsAccount_data = conn.execute(query, {"balance": checkings_param}).fetchall()
-  return checkingsAccount_data
+    uni = session.get('uni', "")
+    #cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    query = text('SELECT ab.accountid, c.balance FROM checkings_account c JOIN account_belongsto ab ON c.accountid = ab.accountid WHERE ab.uni = :uni')
+    params = {'uni': uni}
+    result = g.conn.execute(query, params)
+    g.conn.commit()
 
-#@app.route('/meal_plan', methods=['GET','POST'])
+    for i in result:
+       accountid = i[0]
+       balance = i[1]
+       
+    return render_template("user_profile.html", info = {'accountid': accountid, 'balance': balance})
+
+
+@app.route('/meal_plan', methods=['GET', 'POST'])
 def mealPlan():
-  with engine.connect() as conn:
-    mealplan_param = request.args.get('swipes')
-    query = text("SELECT swipes, dining_dollars, points, flex FROM dining_attachedto WHERE accountid = :accountid")
-    query2= text("SELECT mp.* FROM meal_plan mp JOIN has_plan hp ON mp.mealplanname = hp.mealplanname")
-    mealPlan_data = conn.execute(query, swipes=request.args.get('swipes'), 
-                                   dining_dollars=request.args.get('dining_dollars'), 
-                                    points=request.args.get('points'), flex=request.args.get('flex')).fetchall()
-    mealPlan_data2 = conn.execute(query2, mealplanname = request.args.get('mealplanname')).fetchall()
-    return render_template("user_profile.html", data= mealPlan_data, data2= mealPlan_data2)
+    with engine.connect() as conn:
+        cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-@app.route('/transactions_history', methods=['GET','POST'])
+        cursor.execute("SELECT swipes, dining_dollars, points, flex FROM dining_attachedto WHERE accountid = %s", (session['accountid'],))
+        dinningdata = cursor.fetchone()
+
+        cursor.execute("SELECT mp.* FROM meal_plan mp JOIN has_plan hp ON mp.mealplanname = hp.mealplanname")
+        mealplanname = cursor.fetchone()
+
+    # Check if dinningdata is not None before accessing its values
+    if dinningdata:
+        dict_data = {'swipes': dinningdata['swipes'], 'dining_dollars': dinningdata['dining_dollars'], 'points': dinningdata['points'], 'flex': dinningdata['flex'], 'mealplanname': mealplanname}
+
+        return render_template("user_profile.html", **dict_data)
+
+    # Handle the case where dinningdata is None
+    return render_template("user_profile.html", mealplanname=mealplanname)
+
+
+@app.route('/account_tracking.html', methods=['GET','POST'])
 def transactionHistory():
-  with engine.connect() as conn:
-    if request.method == 'POST':
-      query = text("SELECT * FROM transaction_acchis WHERE accountid = :accountid")
-      transaction_data = conn.execute(query, accountid=request.args.get('accountid')).fetchall()
-      return render_template("account_tracking.html", data= transaction_data)
+    uni = session.get('uni', "")
+    query = text('SELECT * FROM transaction_acchis ta JOIN account_belongsto ab ON ta.accountid = ab.accountid AND ab.uni = :uni')
+    params = {'uni': uni}
+    result = g.conn.execute(query, params)
+    g.conn.commit()
+    
+    for i in result:
+       transactionid = i[0]
+       dateoftransaction = i[1]
+       amount = i[2]
+       location = i[3]
+       purpose = i[4]
+       accountid = i[5]
+       typ = i[6]
+
+    return render_template("account_tracking.html", dict = {'transactionid': transactionid, 'dateoftransaction':dateoftransaction, 'amount': amount, 'location': location, 'purpose': purpose, 'accountid': accountid, 'typ': typ} )
+
 
 if __name__ == "__main__":
   import click
@@ -169,5 +260,3 @@ if __name__ == "__main__":
     app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
 
   run()
-
-
